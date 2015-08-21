fb36cf4cd4c1e9c1c995914eaf32be9ad7962ce5
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-08-21 18:43:14 +0900

    Clear DIRECTORIES

---
+++ 4.js
@@ -0,0 +1,25 @@
+var Hapi = require('hapi');
+var Inert = require('inert');
+var Path = require('path');
+var server = new Hapi.Server();
+
+server.register(Inert, function(err) {
+  if (err) throw err;
+});
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+server.route({
+  path: '/foo/bar/baz/{param}',
+  method:'GET',
+  handler: {
+    directory: {
+      path: Path.join(__dirname, 'public') 
+    }
+  }
+});
+
+server.start(function(){});
