cf2b0f3273d42d2f1369bfd66a3c828bb73efa16
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-08-22 20:21:30 +0900

    Clear VIEWS

---
+++ 5.js
@@ -0,0 +1,34 @@
+var Hapi = require('hapi');
+var Inert = require('inert');
+var Path = require('path');
+var Vision = require('vision');
+var server = new Hapi.Server();
+
+server.register(Inert, function(err) {
+  if (err) throw err;
+});
+server.register(Vision, function (err) {
+  if (err) throw err;
+});
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+server.route({
+  path: '/',
+  method:'GET',
+  handler: {
+    view: 'index.html'
+  }
+});
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: Path.join(__dirname, 'templates')
+});
+
+server.start(function(){});
